b85007462db1c28c2ec90fb301a8bec59aeb3dcf
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-18 20:31:41 +0300

    add test base for project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,13 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+
 public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+    }
 }

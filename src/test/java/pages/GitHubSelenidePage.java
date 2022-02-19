1816ae37e196dfdfea4cd3bab5c9267003b510f9
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-19 14:23:49 +0300

    ade changes: Deleted the github folder, finalized checks

--- src/test/java/pages/GitHubSelenidePage.java
+++ src/test/java/pages/GitHubSelenidePage.java
@@ -1,13 +1,44 @@
 package pages;
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
 
-public class GitHubPage {
+public class GitHubSelenidePage {
     //locators
-
+    final private SelenideElement
+        headerTitle = $("#repository-container-header"),
+        wikiTab = $("#wiki-tab"),
+        headerWiki = $(byText("Welcome to the selenide wiki!")),
+        wikiInputForSearch = $("#wiki-pages-filter"),
+        wikiBox = $("#wiki-pages-box"),
+        btnSoftAssertions = $x(".//a[text()='SoftAssertions']"),
+        repoWrap = $(".repository-content "),
+        wrap = $(".markdown-body");
 
     // actions
-    public GitHubPage openPage() {
+    public GitHubSelenidePage openPage() {
         open("selenide/selenide");
+        headerTitle.shouldHave(text("selenide"));
+        return this;
     }
 
+    public GitHubSelenidePage goToWiki() {
+        wikiTab.click();
+        headerWiki.shouldHave(text("Welcome to the selenide wiki!"));
+        return this;
+    }
+
+    public GitHubSelenidePage searchSoftAssertions() {
+        wikiInputForSearch.setValue("SoftAssertions").pressEnter();
+        wikiBox.shouldHave(text("SoftAssertions"));
+        btnSoftAssertions.click();
+        return this;
+    }
+
+    public GitHubSelenidePage checkJunit5InSoftAssertions() {
+        repoWrap.shouldHave(text("SoftAssertions"));
+        wrap.shouldHave(text("JUnit5 extension"));
+        return this;
+    }
 }
--- src/test/java/tests/SoftAssertionsInJunitTest.java
+++ src/test/java/tests/SoftAssertionsInJunitTest.java
@@ -1,2 +1,16 @@
-package tests;public class SoftAssertionsInJunitTest {
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.GitHubSelenidePage;
+
+public class SoftAssertionsInJunitTest extends TestBase {
+    GitHubSelenidePage gitHubSelenidePage = new GitHubSelenidePage();
+
+    @Test
+    void successReserchTest() {
+        gitHubSelenidePage.openPage()
+                .goToWiki()
+                .searchSoftAssertions()
+                .checkJunit5InSoftAssertions();
+    }
 }

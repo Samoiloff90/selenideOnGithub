b4965b6d82da926a73ed544f1c586b50316aeb48
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-19 14:22:17 +0300

    ade changes: Deleted the github folder, finalized checks

---
+++ src/test/java/pages/GitHubSelenidePage.java
@@ -0,0 +1,13 @@
+package pages;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubPage {
+    //locators
+
+
+    // actions
+    public GitHubPage openPage() {
+        open("selenide/selenide");
+    }
+
+}
